0f7f5a030416b6dc682209867a43b60a6493a65f
olivia l <olivialu415@gmail.com>
2025-09-10 07:32:04 -0700

    Update test.py

--- test.py
+++ test.py
@@ -4,7 +4,33 @@ t = Turtle()
 
 
 t.shape('turtle')
+t.speed = 0.001
 
-t.forward(200)
-lioasdjfhgiop;afk
-turtle.done()
+
+
+def triangle(length):
+    t.forward(length)
+    t.right(120)
+    t.forward(length)
+    t.right(120)
+  
+def square(length):
+    t.forward(length)
+    t.right(90)
+    t.forward(length)
+    t.right(90)
+    t.forward(length)
+    t.right(90)
+   
+def squareSpiral(irange):
+    size = 5
+    for i in range(irange):
+        square(size)
+        size += 15
+        t.right(5)
+
+squareSpiral(60)
+
+for i in range(4):
+    t.forward(100)
+    t.left(90)

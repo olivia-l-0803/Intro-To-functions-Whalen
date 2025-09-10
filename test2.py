e8252e1e6ccc89398592318848d46d867b09cd87
olivia l <olivialu415@gmail.com>
2025-09-10 07:32:00 -0700

    Create test2.py

---
+++ test2.py
@@ -0,0 +1,44 @@
+import turtle
+from turtle import *
+t = Turtle()
+
+t.speed(20)
+t.shape("turtle")
+
+def square(length):
+    t.forward(length)
+    t.right(90)
+    t.forward(length)
+    t.right(90)
+    t.forward(length)
+    t.right(90)
+    t.forward(length)
+    t.right(90)
+   
+def squareSpiral(irange, size):
+    for i in range(irange):
+        t.right(5)
+        square(size)
+        size += 5
+        
+#squareSpiral(60, 10)
+
+def star(libg):
+    t.forward(libg)
+    t.right(144)
+    t.forward(libg)
+    t.right(144)
+    t.forward(libg)
+    t.right(144)
+    t.forward(libg)
+    t.right(144)
+    t.forward(libg)
+    t.right(144)
+
+def starSpiral(monkey, banana):
+    for i in range(monkey):
+        t.right(5)
+        star(banana)
+        banana += 5
+
+starSpiral(60, 6)
